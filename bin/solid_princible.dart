998a0703bd6d2b189dbd9c66134bca87c1715439
prAhmed20 <ttbyqatandrwydmhkrh@gmail.com>
2023-01-05 12:25:28 +0200

    Initial commit

---
+++ bin/solid_princible.dart
@@ -0,0 +1,5 @@
+import 'package:solid_princible/solid_princible.dart' as solid_princible;
+
+void main(List<String> arguments) {
+  print('Hello world: ${solid_princible.calculate()}!');
+}
---
+++ test/solid_princible_test.dart
@@ -0,0 +1,8 @@
+import 'package:solid_princible/solid_princible.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
